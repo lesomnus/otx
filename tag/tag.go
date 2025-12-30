3d742fa30fcb43ccabfc91c2ba42ed136e1933bd
Seunghyun Hwang <lesomnus@gmail.com>
2025-12-30 04:05:26 +0000

    upgrade deps
    Signed-off-by: Seunghyun Hwang <lesomnus@gmail.com>

--- tag/tag.go
+++ tag/tag.go
@@ -2,26 +2,13 @@ package tag
 
 import (
 	"log/slog"
-	"time"
+
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
 
-const TitleKey = "app.widget.name"
+const TitleKey = string(semconv.AppWidgetNameKey)
 
 func Title(title string) slog.Attr {
 	a := slog.String(TitleKey, title)
-	return a
-}
-
-const IngressKey = "internal.otx.ingress"
-
-func Ingress(method string) slog.Attr {
-	a := slog.Group(IngressKey, slog.String("method", method))
-	return a
-}
-
-const EgressKey = "internal.otx.egress"
-
-func Egress(elapsed time.Duration) slog.Attr {
-	a := slog.Group(EgressKey, slog.Duration("elapsed", elapsed))
 	return a
 }
